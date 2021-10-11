17dcf8e9add27f3312a257fde2fe3b0be2c3623c
kgbeast0 <bogdaniura@protonmail.com>
2021-10-11 20:11:14 +0300

    1

---
+++ C++/greedy/main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+struct obiect
+{
+    int k;         //eticheta
+    float g, p, e; //greutate, profit, eficienta
+};
+
+ifstream fin("greedy.in");
+obiect a[1001], aux;
+int n, profit;
+float G, Gr, x[1001];
+
+void citire()
+{
+    cin >> n >> G;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i].g >> a[i].p;
+        a[i].e = a[i].p / a[i].g;
+    }
+}
+
+void sort()
+{
+    for (int j = n; j > 1; j--)
+    {
+        for (int i = 1; i <= j - 1; i++)
+        {
+            if (a[i].e < a[i + 1].e)
+            {
+                swap(a[i], a[i + 1]);
+            }
+        }
+    }
+}
+
+void greedy()
+{
+    Gr = G;
+    for (int i = 1; (i <= n) and Gr > 0; i++)
+    {
+        if (a[i].g <= Gr)
+        {
+            Gr -= a[i].g;
+            x[i] = 1;
+        }
+        else
+        {
+            x[i] = Gr / a[i].g;
+            Gr = 0;
+        }
+        profit += a[i].p * x[i];
+    }
+}
+
+void afisare()
+{
+    cout << profit;
+}
+
+int main()
+{
+    citire();
+    sort();
+    greedy();
+    afisare();
+    
+
+    return 0;
+}
