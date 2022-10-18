0c817298af1f097e3ec21b1d8849d0e8640da30f
kgbeast0 <bogdaniura@protonmail.com>
2022-10-18 16:12:40 +0300

    1

--- python/laborator/operatori.py
+++ python/laborator/operatori.py
@@ -45,10 +45,11 @@ def ariedr():
 
 
 if __name__ == '__main__':
+    print('10%3 -> ', 10%3)
     # suma()
     # diferenta()
     # inmultire()
     # impartire()
     # arie()
     # perimetru()
-    ariedr()
+    # ariedr()
