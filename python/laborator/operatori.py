ea8c8f960def134af75634bf249549a4351b2122
kgbeast0 <bogdaniura@protonmail.com>
2022-10-18 15:31:32 +0300

    1

--- python/laborator/operatori.py
+++ python/laborator/operatori.py
@@ -1,25 +1,54 @@
 def suma():
+    a = input("Introduceti primul numar: ")
+    b = input("Introduceti al doilea numar: ")
     print("Suma este: ", int(a) + int(b))
 
 
 def diferenta():
-
+    a = input("Introduceti primul numar: ")
+    b = input("Introduceti al doilea numar: ")
     print("Diferenta este: ", int(a)-int(b))
 
 
 def impartire():
+    a = input("Introduceti primul numar: ")
+    b = input("Introduceti al doilea numar: ")
     print("Rezultatul impartirii este: ", int(a) / int(b))
 
 
 def inmultire():
-    print("Produsul este: ", int(a) * int(b))
-
-if __name__ == '__main__':
     a = input("Introduceti primul numar: ")
     b = input("Introduceti al doilea numar: ")
-    suma()
-    diferenta()
-    inmultire()
-    impartire()
+    print("Produsul este: ", int(a) * int(b))
 
-    
+
+def arie():
+    c1 = input("Introduceti primul numar: ")
+    c2 = input("Introduceti al doilea numar: ")
+    aria = int(c1) * int(c2) / 2
+    print("Aria este: ", aria)
+
+
+def perimetru():
+    l1 = input("Introduceti primul numar: ")
+    l2 = input("Introduceti al doilea numar: ")
+    l3 = input("Introduceti al treilea numar: ")
+    perimetru = int(l1) + int(l2) + int(l3)
+    print("Perimetrul este: ", perimetru)
+
+
+def ariedr():
+    l1 = input("Introduceti primul numar: ")
+    l2 = input("Introduceti al doilea numar: ")
+    ariedr = int(l1) * int(l2)
+    print("Perimetrul dreptunghiului este: ", ariedr)
+
+
+if __name__ == '__main__':
+    # suma()
+    # diferenta()
+    # inmultire()
+    # impartire()
+    # arie()
+    # perimetru()
+    ariedr()
