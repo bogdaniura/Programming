7c70693c397392b0862396e33132cc2a02923d16
kgbeast0 <bogdaniura@protonmail.com>
2022-10-18 15:19:55 +0300

    1

--- python/laborator/operatori.py
+++ python/laborator/operatori.py
@@ -10,13 +10,12 @@ def diferenta():
 def impartire():
     print("Rezultatul impartirii este: ", int(a) / int(b))
 
+
 def inmultire():
     print("Produsul este: ", int(a) * int(b))
 
 if __name__ == '__main__':
-    # print("Introduceti primul numar: ")
     a = input("Introduceti primul numar: ")
-    # print("Introduceti al doilea numar: ")
     b = input("Introduceti al doilea numar: ")
     suma()
     diferenta()
